3a16609bac9c120f253e67bca3fb6e33d7aa8f5f
Kirill Rasuli <rasulikv@gmail.com>
2020-08-18 16:13:32 +0300

    Connect dialogflow to vk bot

--- vk_bot.py
+++ vk_bot.py
@@ -3,22 +3,30 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
 import os
 import random
+from tg_bot import detect_intent_text
 
 
-def echo(event, vk_api):
+def answer(event, vk_api, project_id):
+    answer = detect_intent_text(
+        project_id,
+        event.user_id,
+        event.text,
+        'ru'
+    )
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+        message=answer,
+        random_id=random.randint(1, 1000)
     )
 
 
 if __name__ == "__main__":
     load_dotenv()
     vk_group_token = os.getenv('VK_GROUP_TOKEN')
+    google_project_id = os.getenv('GOOGLE_PROJECT_ID')
     vk_session = vk_api.VkApi(token=vk_group_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            answer(event, vk_api, google_project_id)
